9931072eaa18b09cf9482939088945271d0fc905
wplnancy <1138627421@qq.com>
2017-10-13 18:22:28 +0800

    Add some example.

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,10 +6,70 @@ Page({
   data: {
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    linkList: [
+      {
+        url: "../sliders/sliders",
+        name: "swiper 滑动容器"
+      },
+      {
+        url: "../icon/icon",
+        name: "icon 图标"
+      },
+      {
+        url: "../text/text",
+        name: "tetx 文本"
+
+      },
+      {
+        url: "../progress/progress",
+        name: "progress 进度条"
+
+      },
+      {
+        url: "../button/button",
+        name: "button 按"
+      },
+      {
+        url: "../checkbox/checkbox",
+        name: "checkbox 多选项"
+      },
+      {
+        url: "../form/form",
+        name: "form 表单"
+      },
+      {
+        url: "../input/input",
+        name: "input 输入框"
+      },
+      {
+        url: "../label/label",
+        name: "label 标签"
+      },
+      {
+        url: '../picker/picker',
+        name: 'picker 滚动选择'
+      },
+      {
+        url: '../radio/radio',
+        name: 'radio 单选项目'
+      },
+      {
+        url: '../slider/slider',
+        name: 'slider 滑动选择器'
+      },
+      {
+        url: '../switch/switch',
+        name: 'switch 开关选择器'
+      },
+      {
+        url: '../textarea/textarea',
+        name: 'textarea 多行输入框'
+      }
+    ]
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
@@ -20,7 +80,7 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -42,7 +102,7 @@ Page({
       })
     }
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
--- pages/label/label.js
+++ pages/label/label.js
@@ -1,3 +1,45 @@
-// pages/label/label.js
 Page({
+  data: {
+    checkboxItems: [
+      { name: 'ctrip', value: '携程', checked: 'true' },
+      { name: 'qunar', value: '去哪儿' },
+      { name: 'tuniu', value: '途牛' }
+    ],
+    radioItems: [
+      { name: 'ctrip', value: '携程' },
+      { name: 'qunar', value: '去哪儿', checked: 'true' },
+      { name: 'tuniu', value: '途牛' }
+    ],
+    hidden: false
+  },
+  checkboxChange: function (e) {
+    var checked = e.detail.value
+    var changed = {}
+    for (var i = 0; i < this.data.checkboxItems.length; i++) {
+      if (checked.indexOf(this.data.checkboxItems[i].name) !== -1) {
+        changed['checkboxItems[' + i + '].checked'] = true
+      } else {
+        changed['checkboxItems[' + i + '].checked'] = false
+      }
+    }
+    this.setData(changed)
+  },
+  radioChange: function (e) {
+    var checked = e.detail.value
+    var changed = {}
+    for (var i = 0; i < this.data.radioItems.length; i++) {
+      if (checked.indexOf(this.data.radioItems[i].name) !== -1) {
+        changed['radioItems[' + i + '].checked'] = true
+      } else {
+        changed['radioItems[' + i + '].checked'] = false
+      }
+    }
+    this.setData(changed)
+  },
+  testLabelBindButton_1: function () {
+    console.log("奔走相告，button通过for可以绑定成功啦！！！");
+  },
+  testLabelBindButton_2: function () {
+    console.log("奔走相告，button通过内嵌可以绑定成功啦！！！");
+  }
 })
